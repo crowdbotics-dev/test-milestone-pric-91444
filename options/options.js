98a42c3aedaa42b9d3e8575dd357722294d287aa
Crowdbotics <engineering@crowdbotics.com>
2023-06-14 11:52:48 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "test-milestone-pric-91444",
+  url: "https://test-milestone-pric-91444.botics.co",
+  api: "https://test-milestone-pric-91444.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
